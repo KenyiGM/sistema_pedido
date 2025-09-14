ce7aaa4953ff760782e04c8ac8a22cb11f598e6f
KenyiGM <sunted351@gmail.com>
2025-09-14 13:12:51 -0500

    Creacion de la interfaz del sistema de pedido

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # sistema_pedido
+
+## Problema:
+
+Una empresa dedicada a la venta de productos al por menor necesita un sistema que permita registrar cada pedido realizado por sus clientes.
+
+- La gerencia ha solicitado que el sistema pueda calcular el monto total a pagar por cada pedido.
+
+- En caso de que un cliente adquiera una cantidad considerable de un mismo producto, se le aplique automáticamente un descuento especial.
+
+- Además, al final de la jornada, el sistema debe generar un resumen con el total de ventas realizadas durante el día.
+
+## Solucion:
+
+### Analizamos el proceso:
+
+1. Un cliente hace un pedido.
+2. El recepcionista atiende el pedido.
+3. Crea un pedido en el sistema.
+4. Agregar los productos al pedido en el sistema.
+5. Calcular descuento de los productos en el sistema.
+6. Calcular el monto a pagar en el sistema.
+7. Genera Boleta o Factura del pedido.
+8. Entrega Boleta o Factura.
+9. Al final del dia ve el resumen total de ventas realizadas durante el dia en el sistema.
--- main.py
+++ main.py
@@ -1,1 +1,59 @@
-print("Hello World")
+from datetime import datetime, timezone
+import typer
+from rich import print
+from rich.table import Table
+
+app = typer.Typer()
+
+@app.command()
+def main():
+    while True:
+        print("Interfaz de Pedidos y Ventas")
+        print("1. Crear Pedido")
+        print("2. Ver Resumen de Ventas")
+        print("3. Salir")
+
+        opcion_principal = typer.prompt("Ingrese una opción: ", type=int)
+
+        match opcion_principal:
+            case 1:
+                print("Crear Pedido")
+                print("1. Agregar Producto")
+                print("2. Finalizar Pedido")
+                print("3. Cancelar Pedido")
+                opcion_secundaria = typer.prompt("Ingrese una opción: ", type=int)
+                match opcion_secundaria:
+                    case 1:
+                        print("Agregar Producto")
+                        tabla_productos = Table(title="Productos Disponibles")
+                        tabla_productos.add_column("ID")
+                        tabla_productos.add_column("Nombre")
+                        tabla_productos.add_column("Precio")
+                        tabla_productos.add_column("Stock")
+                        tabla_productos.add_row()
+                        tabla_productos.add_row()
+                        print(tabla_productos)
+                        producto_id = typer.prompt("Ingrese el ID del producto: ", type=int)
+                        producto_cantidad = typer.prompt("Ingrese la cantidad del producto: ", type=int)
+                        print("Producto agregado con éxito")
+                    case 2:
+                        print("Pedido creado con éxito")
+                    case 3:
+                        print("Pedido cancelado")
+                    case _:
+                        print("Opción no válida")
+            case 2:
+                print("Resumen de Ventas")
+                print("Pedidos realizados hoy: 20")
+                print("Total de ventas: S/500.00")
+                print("Producto mas vendido: Producto 1 (10 ventas)")
+                print("Producto menos vendido: Producto 2 (5 ventas)")
+            case 3:
+                print("Salir")
+                False
+                break 
+            case _:
+                print("Opción no válida")
+
+if __name__ == "__main__":
+    app()
